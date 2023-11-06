34ac1dd1b11dd97118aacae9a749cccf04e44789
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-06 16:48:51 +0100

    [ Init ] db for array new movies

---
+++ db/db.php
@@ -0,0 +1,7 @@
+<?php
+
+$movies = [
+  new Movie('Nuovo Olimpo','Ferzan Özpetek',['drammatico','sentimentale'],2023,120,'italiano'),
+  new Movie('The Wonderful Story of Henry Sugar','Wes Anderson',['drammatico','commedia','avventura'],2023,41,'inglese'),
+  new Movie('Pain Hustlers ','David Yates',['drammatico','poliziesco','thriller'],2023,122,'inglese')
+];
