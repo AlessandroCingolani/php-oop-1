ccffcba7e0e35f40d937b6bfbb89740a15991886
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-06 16:49:18 +0100

    [ add ] foreach and cicle imported db

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once __DIR__ . '/db/db.php';
+
 class Movie{
   public $title;
   public $author;
@@ -7,6 +10,7 @@ class Movie{
   public $duration;
   public $language;
 
+  // construct con i parametri richiesti
   public function __construct(string $_title,string $_author,array $_genre,int $_year,int $_duration,string $_language){
     $this->title = $_title;
     $this->author = $_author;
@@ -16,6 +20,7 @@ class Movie{
     $this->language = $_language;
   }
 
+  // funzione per ottere ore minuti dai minuti totali forniti
   public function convertMinute($_duration){
     $this->duration = $_duration;
     $hours = floor($_duration / 60);
@@ -30,9 +35,6 @@ class Movie{
 
 }
 
-$nuovo_olimpo = new Movie('Nuovo Olimpo','Ferzan Özpetek',['drammatico','sentimentale'],2023,120,'italiano');
-var_dump($nuovo_olimpo);
-
 ?>
 
 <!DOCTYPE html>
@@ -40,9 +42,27 @@ var_dump($nuovo_olimpo);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <title>OOP-1</title>
 </head>
 <body>
-  
+  <div class="container d-flex mt-5">
+    <?php
+    foreach($movies as $movie):
+    ?>
+
+    <div class="card m-5" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+      <h4 class="card-title"><?php echo $movie->title  ?></h4>
+      <h5><?php echo $movie->author  ?></h5>
+      <h6 class="text-success"><?php echo implode(", " , $movie->genre)  ?></h6>
+      <strong>Year: <?php echo $movie->year  ?></strong>
+      <p>Duration: <?php echo $movie->duration  ?></p>
+      <p>Language: <?php echo $movie->language  ?></p>
+      </div>
+    </div>
+    <?php endforeach; ?>
+  </div>
 </body>
 </html>
