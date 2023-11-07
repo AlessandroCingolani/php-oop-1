64e14c125d509e7c1ae6c3812c9d1be92996c79e
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-07 14:30:50 +0100

    [ Init ] Movie with unic params and parents param

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,39 +1,16 @@
 <?php
 
-class Movie{
-  public $image;
-  public $title;
-  public $author;
-  public $genre;
-  public $year;
-  public $duration;
-  public $language;
+class Movie extends Production {
+  public $published_year;
+  public $running_time;
 
   // construct con i parametri richiesti
-  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,int $_year,int $_duration,string $_language){
-    $this->title = $_title;
-    $this->author = $_author;
-    $this->genre = $_genre;
-    $this->year = $_year;
-    $this->convertMinute($_duration);
-    $this->language = $_language;
+  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,string $_language, int $_published_year,int $_running_time){
+    parent::__construct($_image,$_title,$_author,$_genre,$_language);
+    $this->published_year = $_published_year;
+    $this->running_time = $_running_time;
   }
 
-  // funzione per ottere ore minuti dai minuti totali forniti
-  public function convertMinute($_duration){
-    $this->duration = $_duration;
-    $hours = floor($_duration / 60);
-    $minutes = $_duration % 60;
-    if($this->duration < 1){
-      $converter = substr($this->duration, strpos($this->duration, '.') + 1);
-      $this->duration = "$converter Min";
-    }
-    $this->duration = $hours . "h " . $minutes . "m";
-  }
-
-  //  setto immagine
-  public function setImage(Media $_image){
-    $this->image = $_image;
-  }
+ 
 
 }
