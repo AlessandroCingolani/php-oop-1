f2f210bcaba655f679bdf1a9993dda6fc68d0d09
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-07 14:47:32 +0100

    [ del ] aired to year params in the construct

--- Model/TvSerie.php
+++ Model/TvSerie.php
@@ -7,10 +7,10 @@ class TvSerie extends Production {
   public $number_of_seasons;
 
   // construct con i parametri richiesti
-  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,string $_language, string $_aired_from_year,int $_number_of_episodes,int $_number_of_seasons){
-    parent::__construct($_image,$_title,$_author,$_genre,$_language);
+  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,array $_actors,string $_language, string $_aired_from_year,int $_number_of_episodes,int $_number_of_seasons){
+    parent::__construct($_image,$_title,$_author,$_genre,$_actors,$_language);
     $this->aired_from_year = $_aired_from_year;
-    $this->aired_to_year = $_aired_to_year;
+    //$this->aired_to_year = $_aired_to_year;
     $this->number_of_episodes = $_number_of_episodes;
     $this->number_of_seasons = $_number_of_seasons;
   }
