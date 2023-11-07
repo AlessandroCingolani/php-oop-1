c2e525d4d8cf27e38e614b8c05105b6a65e574b9
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-07 16:02:05 +0100

    [ change ] string to int params

--- Model/TvSerie.php
+++ Model/TvSerie.php
@@ -7,7 +7,7 @@ class TvSerie extends Production {
   public $number_of_seasons;
 
   // construct con i parametri richiesti
-  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,array $_actors,string $_language, string $_aired_from_year, $_aired_to_year,int $_number_of_episodes,int $_number_of_seasons){
+  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,array $_actors,string $_language, int $_aired_from_year, $_aired_to_year,int $_number_of_episodes,int $_number_of_seasons){
     parent::__construct($_image,$_title,$_author,$_genre,$_actors,$_language);
     $this->aired_from_year = $_aired_from_year;
     $this->aired_to_year = $_aired_to_year;
