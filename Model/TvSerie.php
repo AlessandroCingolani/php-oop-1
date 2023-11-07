274cfa87140aa225ffa7c21e1e6ad20fb0f7eba6
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-07 14:31:13 +0100

    [ Init ] TvSerie with unic params and parents par

---
+++ Model/TvSerie.php
@@ -0,0 +1,20 @@
+<?php
+
+class TvSerie extends Production {
+  public $aired_from_year;
+  public $aired_to_year;
+  public $number_of_episodes;
+  public $number_of_seasons;
+
+  // construct con i parametri richiesti
+  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,string $_language, string $_aired_from_year,int $_number_of_episodes,int $_number_of_seasons){
+    parent::__construct($_image,$_title,$_author,$_genre,$_language);
+    $this->aired_from_year = $_aired_from_year;
+    $this->aired_to_year = $_aired_to_year;
+    $this->number_of_episodes = $_number_of_episodes;
+    $this->number_of_seasons = $_number_of_seasons;
+  }
+
+ 
+
+}
