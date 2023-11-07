e334a4d4ab747a4510d10f8619a3a1830e679caa
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-07 15:09:46 +0100

    [ add ] pseudo data for see working loop

--- db/db.php
+++ db/db.php
@@ -1,7 +1,10 @@
 <?php
 
-// $product = [
-//   new Movie(new Media('', 'Copertina'),'Nuovo Olimpo','Ferzan Özpetek',['drammatico','sentimentale'],2023,120,'italiano'),
-//   new Movie(new Media('', 'Copertina'),'The Wonderful Story of Henry Sugar','Wes Anderson',['drammatico','commedia','avventura'],2023,41,'inglese'),
-//   new Movie(new Media('', 'Copertina'),'Pain Hustlers ','David Yates',['drammatico','poliziesco','thriller'],2023,122,'inglese')
-// ];
+$media_list = [
+  new Movie(new Media('', 'Copertina'),'Nuovo Olimpo','Ferzan Özpetek',['drammatico','sentimentale'],['asd','ciao'],'italiano',2021,112),
+  new TvSerie(new Media('', 'Copertina'),'Pokemon','Ash',['drammatico','sentimentale'],['Misty','Brok','Ash'],'italiano','Settembre 1990',null,100,7),
+  new TvSerie(new Media('', 'Copertina'),'Pokemon','Ash',['drammatico','sentimentale'],['Misty','Brok','Ash'],'italiano','Settembre 1990',null,100,7),
+  new Movie(new Media('', 'Copertina'),'The Wonderful Story of Henry Sugar','Wes Anderson',['drammatico','commedia','avventura'],['asd','ciao'],'inglese',2021,112),
+  new Movie(new Media('', 'Copertina'),'Pain Hustlers ','David Yates',['drammatico','poliziesco','thriller'],['asd','ciao'],'italiano',2021,112),
+  new TvSerie(new Media('', 'Copertina'),'Pokemon','Ash',['drammatico','sentimentale'],['Misty','Brok','Ash'],'italiano','Settembre 1990',null,100,7)
+];
