7c6b5c0487d303c6abe6dd73a9d6e2ff4b99a866
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-07 16:01:38 +0100

    [ add ] details film and series

--- db/db.php
+++ db/db.php
@@ -1,10 +1,10 @@
 <?php
 
 $media_list = [
-  new Movie(new Media('', 'Copertina'),'Nuovo Olimpo','Ferzan Özpetek',['drammatico','sentimentale'],['asd','ciao'],'italiano',2021,112),
-  new TvSerie(new Media('', 'Copertina'),'Pokemon','Ash',['drammatico','sentimentale'],['Misty','Brok','Ash'],'italiano','Settembre 1990',null,100,7),
-  new TvSerie(new Media('', 'Copertina'),'Pokemon','Ash',['drammatico','sentimentale'],['Misty','Brok','Ash'],'italiano','Settembre 1990',null,100,7),
-  new Movie(new Media('', 'Copertina'),'The Wonderful Story of Henry Sugar','Wes Anderson',['drammatico','commedia','avventura'],['asd','ciao'],'inglese',2021,112),
-  new Movie(new Media('', 'Copertina'),'Pain Hustlers ','David Yates',['drammatico','poliziesco','thriller'],['asd','ciao'],'italiano',2021,112),
-  new TvSerie(new Media('', 'Copertina'),'Pokemon','Ash',['drammatico','sentimentale'],['Misty','Brok','Ash'],'italiano','Settembre 1990',null,100,7)
+  new Movie(new Media('img/nuovo-olimpo.webp', 'Copertina'),'Nuovo Olimpo','Ferzan Özpetek',['drammatico','sentimentale'],['Damiano Gavino','Andrea Di Luigi','Greta Scarano'],'italiano',2023,112),
+  new TvSerie(new Media('img/stranger-things.webp', 'Copertina'),'Stranger Things',' Duffer Brothers',['drammatico','fantascienza','orrore','thriller'],['Sean Astin','Paul Reiser','Winona Ryder','David Harbour'],'inglese',2016,null,34,5),
+  new TvSerie(new Media('img/one-piece.webp', 'Copertina'),'One Piece','Eiichirō Oda',['shōnen'],['Monkey D. Rufy',' Roronoa Zoro','Nami'],'giapponese',1997,null,1082,20),
+  new Movie(new Media('', 'Copertina'),'The Wonderful Story of Henry Sugar','Wes Anderson',['drammatico','commedia','avventura'],['Benedict Cumberbatch','Ralph Fiennes','Dev Patel'],'inglese',2023,39),
+  new Movie(new Media('', 'Copertina'),'Pain Hustlers ','David Yates',['drammatico','poliziesco','thriller'],['Emily Blun','Chris Evans','Catherine O Hara'],'italiano',2023,122),
+  new TvSerie(new Media('', 'Copertina'),'The Umbrella Academy',' Steve Blackman',['commedia nera', 'drammatico', 'fantascienza','supereroi','fantastico'],['Elliot Page','Tom Hopper',' David Castañeda',' Emmy Raver-Lampman'],'inglese',2019,null,30,4)
 ];
