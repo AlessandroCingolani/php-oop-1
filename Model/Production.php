0ffc10fa56cc14b6f19b75e47409e8d0ed7f2b76
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-07 16:36:02 +0100

    [ del ] comments

--- Model/Production.php
+++ Model/Production.php
@@ -15,21 +15,10 @@ class Production{
     $this->author = $_author;
     $this->genre = $_genre;
     $this->actors = $_actors;
-    //$this->convertMinute($_duration);
     $this->language = $_language;
   }
 
-  // funzione per ottere ore minuti dai minuti totali forniti
-  // public function convertMinute($_duration){
-  //   $this->duration = $_duration;
-  //   $hours = floor($_duration / 60);
-  //   $minutes = $_duration % 60;
-  //   if($this->duration < 1){
-  //     $converter = substr($this->duration, strpos($this->duration, '.') + 1);
-  //     $this->duration = "$converter Min";
-  //   }
-  //   $this->duration = $hours . "h " . $minutes . "m";
-  // }
+
 
   //  setto immagine
   public function setImage(Media $_image){
