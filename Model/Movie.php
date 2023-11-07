c38f2fbd35c4773ec4efdd9d020650fdcb0d1be3
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-07 14:46:22 +0100

    [ add ] Actors

--- Model/Movie.php
+++ Model/Movie.php
@@ -5,8 +5,8 @@ class Movie extends Production {
   public $running_time;
 
   // construct con i parametri richiesti
-  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,string $_language, int $_published_year,int $_running_time){
-    parent::__construct($_image,$_title,$_author,$_genre,$_language);
+  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,array $_actors,string $_language, int $_published_year,int $_running_time){
+    parent::__construct($_image,$_title,$_author,$_genre,$_actors,$_language);
     $this->published_year = $_published_year;
     $this->running_time = $_running_time;
   }
