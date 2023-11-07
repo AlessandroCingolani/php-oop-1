d76da3eaef1ccec97ebc188b27d56c2bb2999e3b
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-07 14:46:49 +0100

    [ comment ] db

--- db/db.php
+++ db/db.php
@@ -1,7 +1,7 @@
 <?php
 
-$movies = [
-  new Movie(new Media('', 'Copertina'),'Nuovo Olimpo','Ferzan Özpetek',['drammatico','sentimentale'],2023,120,'italiano'),
-  new Movie(new Media('', 'Copertina'),'The Wonderful Story of Henry Sugar','Wes Anderson',['drammatico','commedia','avventura'],2023,41,'inglese'),
-  new Movie(new Media('', 'Copertina'),'Pain Hustlers ','David Yates',['drammatico','poliziesco','thriller'],2023,122,'inglese')
-];
+// $product = [
+//   new Movie(new Media('', 'Copertina'),'Nuovo Olimpo','Ferzan Özpetek',['drammatico','sentimentale'],2023,120,'italiano'),
+//   new Movie(new Media('', 'Copertina'),'The Wonderful Story of Henry Sugar','Wes Anderson',['drammatico','commedia','avventura'],2023,41,'inglese'),
+//   new Movie(new Media('', 'Copertina'),'Pain Hustlers ','David Yates',['drammatico','poliziesco','thriller'],2023,122,'inglese')
+// ];
