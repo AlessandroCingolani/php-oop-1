560d2a1be9dd613f5a05d702a1c4560a65f4b02a
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-08 14:56:54 +0100

    [ add ] trait aired year

--- Model/Production.php
+++ Model/Production.php
@@ -2,6 +2,8 @@
 
 class Production
 {
+  use AiredYear;
+
   public $image;
   public $title;
   public $author;
@@ -10,12 +12,14 @@ class Production
   public $language;
 
   // construct con i parametri richiesti
-  public function __construct(Media $_image = null, string $_title, string $_author, array $_genre, array $_actors, string $_language)
+  public function __construct(Media $_image = null, string $_title, string $_author, array $_genre, array $_actors, string $_language, int $_year)
   {
     $this->image = $_image;
     $this->title = $_title;
     $this->author = $_author;
     $this->genre = $_genre;
+    $this->year = $_year;
+
 
     // error per array attori vuoto
     if (empty($_actors)) {
