2d4304bf9fff046a358769408ba5ad6bfe464788
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-08 14:56:41 +0100

    [ add ] parent trait function

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,16 +1,15 @@
 <?php
 
-class Movie extends Production {
+class Movie extends Production
+{
   public $published_year;
   public $running_time;
 
   // construct con i parametri richiesti
-  public function __construct(Media $_image = null,string $_title,string $_author,array $_genre,array $_actors,string $_language, int $_published_year,int $_running_time){
-    parent::__construct($_image,$_title,$_author,$_genre,$_actors,$_language);
-    $this->published_year = $_published_year;
+  public function __construct(Media $_image = null, string $_title, string $_author, array $_genre, array $_actors, string $_language, int $_year, int $_running_time)
+  {
+    parent::__construct($_image, $_title, $_author, $_genre, $_actors, $_language, $_year);
+    $this->published_year = parent::getYear();
     $this->running_time = $_running_time;
   }
-
- 
-
 }
--- Model/TvSerie.php
+++ Model/TvSerie.php
@@ -8,16 +8,16 @@ class TvSerie extends Production
   public $number_of_seasons;
 
   // construct con i parametri richiesti
-  public function __construct(Media $_image = null, string $_title, string $_author, array $_genre, array $_actors, string $_language, int $_aired_from_year, $_aired_to_year, int $_number_of_episodes, int $_number_of_seasons)
+  public function __construct(Media $_image = null, string $_title, string $_author, array $_genre, array $_actors, string $_language, int $_year, $_aired_to_year, int $_number_of_episodes, int $_number_of_seasons)
   {
-    parent::__construct($_image, $_title, $_author, $_genre, $_actors, $_language);
-
+    parent::__construct($_image, $_title, $_author, $_genre, $_actors, $_language, $_year);
+    $this->aired_from_year = parent::getYear();
     // error per anno di pubblicazione
-    if ($_aired_from_year > 1800 && $_aired_from_year <= date("Y")) {
-      $this->aired_from_year = $_aired_from_year;
-    } else {
-      throw new Exception('Inserire anno pubblicazione veritiero');
-    }
+    // if ($_aired_from_year > 1800 && $_aired_from_year <= date("Y")) {
+    //   $this->aired_from_year = $_aired_from_year;
+    // } else {
+    //   throw new Exception('Inserire anno pubblicazione veritiero');
+    // }
     // $this->aired_from_year = $_aired_from_year;
     $this->aired_to_year = $_aired_to_year;
     $this->number_of_episodes = $_number_of_episodes;
