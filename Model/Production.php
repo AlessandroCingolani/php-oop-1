aa408a60d71cc966e29a171078f4cd879be8702d
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-08 15:29:03 +0100

    [ add ] message error for empty title e author

--- Model/Production.php
+++ Model/Production.php
@@ -15,8 +15,12 @@ class Production
   public function __construct(Media $_image = null, string $_title, string $_author, array $_genre, array $_actors, string $_language, int $_year)
   {
     $this->image = $_image;
-    $this->title = $_title;
-    $this->author = $_author;
+    if (empty($_title) || empty($_author)) {
+      throw new Exception('Non lasciare uno dei seguenti campi vuoti : Title-Author');
+    } else {
+      $this->title = $_title;
+      $this->author = $_author;
+    }
     $this->genre = $_genre;
     $this->year = $_year;
 
